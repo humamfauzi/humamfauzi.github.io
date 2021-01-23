9f4950005704784e9b075dee1db23c3076a5db59
Humam Fauzi <humam.fauzi.93@gmail.com>
2021-01-23 20:19:11 +0700

    add more plottable example

--- _posts/2020-11-07-plottable.md
+++ _posts/2020-11-07-plottable.md
@@ -13,7 +13,8 @@ We use node modules and webpack to insert PlottableJS inside our browser. The re
 
 The ideas is we create our own library using typescript and pack it using webpack so our pages could fetch some function from it. My library is in `scripts/index.ts`. Webpack could embed it in `window` variable which accessible from browser. Our library called `AncillaryLib`.
 
-Like before, we trey to plot a circle with plottable JS.
+#### Basics
+Like before, we try to plot a circle with plottable JS.
 <div class="plot_container" width="800px" height="800px">
 <svg id="example2" width="550px" height="550px" class="plot"></svg>
 </div>
@@ -120,13 +121,15 @@ PlottableJS only accept dataset through class of `Dataset` so any data we have s
 
 We also can modify the property of each component, this will be useful want we want to create an interaction so we could design what triggers change what propertries.
 
+#### Attributes
+
 We use a scatter plot to demonstrate attribute
 
 <div class="plot_container" width="800px" height="800px">
 <svg id="example3" width="550px" height="550px" class="plot"></svg>
 </div>
 <script type="text/javascript">
-  const svgDOM = document.getElementById("example3")
+  var svgDOM = document.getElementById("example3")
   var data = []
   n = 100;
   for (let i = 0; i < n; i++) {
@@ -142,3 +145,175 @@ We use a scatter plot to demonstrate attribute
   });
 </script>
 
+The scatter plot have one more datapoint, size. We set random number as a size, so anytime we refresh the page, the plot will be changed. We apply same `x` and `y` datapoint, same as previous plot. 
+
+Each plottable compenent is based on `svg` tag which stands for Scalable Vector Graphics. PlottableJS generates resizeable plot that ultilize vector so it won't break when zoomed. Like any HTML tags, `svg` also have their own attribute to enrich browsing experience. Attribute can be accesses via `.attr` method which consists of the attribute and the value we want.
+
+```javascript
+var plot = new Plots.Scatter()
+  .addDataset(new Dataset(data))
+  .x(function(d) { return d.x; }, xScale)
+  .y(function(d) { return d.y; }, yScale)
+  .size(function() { return Math.random()*100})
+  // this is for SVG Attributes
+  .attr("fill-opacity", .4)
+```
+
+In the example, we change attribute of `fill-opacity` to reduce opacity of each scatter plot. Full list of available attribute can be found [here](https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute).
+
+If we click the plot, it will change color randomly. `Interactions` can alter color of plot. Our interactions function are
+
+```javascript
+const interaction = new Interactions.Click();
+interaction.onClick(function() {
+  const color = ['red', 'green', 'blue', 'yellow', '#AAAAAA']
+  plot.selections().attr('fill', color[Math.floor(Math.random() * color.length)])
+})
+interaction.attachTo(plot)
+```
+
+There are three part of interaction, first we need to instantiate the type of interactions. Second, we need to declare the function of what happen if the trigger happen. Finally, we need to attach it to the designated plot. In this example, we change the scatter plot color randomly in each click.
+
+#### Non Plots
+
+We can utilize PlottableJS to create non plots. Let's say we want to create a confusion matrix. A confusion matrix, in its simplest form, is a two by two matrix. The first one shows the true positive rate of an categorical prediction. For example, you create a prediction engine that predict a coin toss result where head equal positive and tail equal to negative. If the result and the prediction is the same, then it belongs to true positive. Second one is the false negative, the number when your prediction shows head (equal positive) and tail the result tail (equal negative) then it belongs to second one, the third one is like the second one but reversed and the last one is like first one but both of them are negative.
+
+Confusion matrix will look like this.
+
+<div class="plot_container" width="600px" height="500px">
+<svg id="example4" width="500px" height="400px" class="plot"></svg>
+</div>
+<script type="text/javascript">
+  var svgDOM = document.getElementById("example3")
+  var data = {
+    tpr: 10,
+    fnr: 12,
+    fpr: 15,
+    tnr: 22,
+  }
+  var confMatrix = plotLineInstance.confusionMatrixV2(data, "svg#example4")
+  window.addEventListener("resize", function() {
+    confMatrix.redraw();
+  });
+</script>
+
+Confusion matrix is constructed based on label, numbers and predicted/actual sign is a label. We compose it using group. First composition is the actual matrix itself
+```javascript
+const yLabelAT = new Components.Label(ACTUAL_TRUE, 90)
+const yLabelAF = new Components.Label(ACTUAL_FALSE, 90)  
+
+const xLabelPT = new Components.Label(PRED_TRUE)
+const xLabelPF = new Components.Label(PRED_FALSE)
+
+const padding = 50
+const tprLabel = new Components.Label(tpr.toString()).padding(padding).addClass('modLabel')
+const fnrLabel = new Components.Label(fnr.toString()).padding(padding).addClass('modLabel')
+const fprLabel = new Components.Label(fpr.toString()).padding(padding).addClass('modLabel')
+const tnrLabel = new Components.Label(tnr.toString()).padding(padding).addClass('modLabel')
+const group2 = new Components.Table([
+  [yLabelAT, tprLabel, fprLabel],
+  [yLabelAF, fnrLabel, tnrLabel],
+  [null, xLabelPT, xLabelPF],
+])
+```
+
+we add padding to increase matrix readibility. PlottableJS also support passing down a CSS stylesheet to a label. Each oh the number now have `.modLable` style which alter their font-size. After we compose it, now we will embed it to master plot.
+
+```javascript
+const masterTitle = new Components.TitleLabel("CONFUSION MATRIX")
+const masterChart = new Components.Table([
+  [null, masterTitle],
+  [yLabel, group2],
+  [null, xLabel]
+])
+masterChart.renderTo(frame);
+
+```
+Modifying table is not possible for now since all table in PlottableJS is constructed using a `g` tag and it does not support modification such as adding border or color through CSS. However modifying text and plot is still possbile since basic svg tags such as `text` and `rect` accept CSS styling.
+
+### Interaction
+Plottable also support interaction. Let's see how it works. Let's say we want to generate random scatter plot. So every time we click radomize, the plot will generate different sample.
+<div class="plot_container" width="600px" height="400px">
+<svg id="example5" width="600px" height="300px" class="plot"></svg>
+</div>
+<script type="text/javascript">
+  var confMatrix = plotLineInstance.randomScatter("svg#example5")
+  window.addEventListener("resize", function() {
+    confMatrix.redraw();
+  });
+</script>
+The first problem we could notice here is the scale apparently does not count the scatter size so there is a visible cutoffline when using scatter with variable size.
+
+```javascript
+const colorLabel = new Components.Label("Color")
+const randomizeLabel = new Components.Label("Random")
+const panel = new Components.Table([
+  [colorLabel],
+  [randomizeLabel]
+])
+```
+
+We use labels to create additional button. Later, we attach an interaction to each label. We use click interaction. There are two kind of interaction that we want to present. First, we change the color of the plot, and second, we randomize the value. For the color part, the code is straightoforward.
+```javascript
+const interactionClick2 = new Interactions.Click();
+interactionClick2.onClick(() => {
+  const color = ['#374025', '#D8D9C7', '#A6896F', '#8C594D', '#592B27'];
+  plot.selections().attr('fill', color[Math.floor(Math.random() * color.length)])
+})
+interactionClick2.attachTo(colorLabel)
+```
+
+We set a colorscheme and a random chooser so everytime we click the panel, plot picks random color and change the plot color. Personally, i feel the `.onClick` method is redundant since we already declare we want a click interaction in `new Interactions.Click()`. After we set what happen when we click, we attach it to the label. Same principle goes to randomizer
+```Javascript
+const interactionClick = new Interactions.Click();
+interactionClick.onClick(() => {
+  plot.removeDataset(dataset)
+  dataset = new Dataset(this.generateRandomScatterData(100))
+  plot.addDataset(dataset)
+})
+interactionClick.attachTo(randomizeLabel)
+```
+
+First, we remove current dataset, create a new one, and add it to the plot. The interaction is not as smooth as plotlyjs but for displaying interactive environment, it did well.
+
+### Animation
+
+<div class="plot_container" width="600px" height="500px">
+<svg id="example6" width="500px" height="400px" class="plot"></svg>
+</div>
+<script type="text/javascript">
+  var anim = plotLineInstance.animationScatter("svg#example6")
+  window.addEventListener("resize", function() {
+    anim.redraw();
+  });
+</script>
+
+Here we draw a random scatter plot. Each time we click the plot, it will generate new one. We apply animation in the transition. Animation works in change, in this case, the change is caused by click in the plot. Let's see what happen if we animate a line plot
+
+<div class="plot_container" width="600px" height="500px">
+<svg id="example7" width="500px" height="400px" class="plot"></svg>
+</div>
+<script type="text/javascript">
+  var anim = plotLineInstance.animateLinePlot("svg#example7")
+  window.addEventListener("resize", function() {
+    anim.redraw();
+  });
+</script>
+
+Apparently, animation in line plot is less impressive compared to scatter one. Let's see if bar chart have better animation
+
+<div class="plot_container" width="600px" height="500px">
+<svg id="example8" width="500px" height="400px" class="plot"></svg>
+</div>
+<script type="text/javascript">
+  var anim = plotLineInstance.plotBarAnimation("svg#example8")
+  window.addEventListener("resize", function() {
+    anim.redraw();
+  });
+</script>
+
+Bar has better animation than line. 
+
+### Conclusion
+
+Plottable JS have an intuitive API. Albeit, it has some limitation, for web plotting should be adequate. The only notable limitation is in plot annotations. Hopefully we will see more feature from D3.js translated to Plottable JS. 
--- scripts/plottable_lib.ts
+++ scripts/plottable_lib.ts
@@ -5,7 +5,7 @@ import {
   Plots,
   Dataset,
   Interactions,
-  Interaction
+  Animators
 } from "plottable";
 
 interface LineData {
@@ -67,6 +67,12 @@ export class PlottableLibrary {
       .size(function() { return Math.random()*100})
       // this is for SVG Attributes
       .attr("fill-opacity", .4)
+    const interaction = new Interactions.Click();
+    interaction.onClick(function() {
+      const color = ['red', 'green', 'blue', 'yellow', '#AAAAAA']
+      plot.selections().attr('fill', color[Math.floor(Math.random() * color.length)])
+    })
+    interaction.attachTo(plot)
     
     plot.renderTo(frame);
     return plot
@@ -77,5 +83,290 @@ export class PlottableLibrary {
       new Scales.Linear(),
       new Scales.Linear(),
     ]
+  }
+  private buildXYAxis({xScale, yScale} :{xScale: Scales.Linear, yScale: Scales.Linear}) :[Axes.Numeric, Axes.Numeric] {
+    return [
+      new Axes.Numeric(xScale, "bottom"),
+      new Axes.Numeric(yScale, "left"),
+    ]
+  }
+
+  private buildCategoryScale(xDomain: string[], yDomain: string[]) :[Scales.Category, Scales.Category] {
+    return [
+      new Scales.Category().domain(xDomain),
+      new Scales.Category().domain(yDomain),
+    ]
+  }
+
+  private generateBox([xCat, yCat]:[Scales.Category, Scales.Category]) {
+    return new Plots.Rectangle()
+    .addDataset(new Dataset([{x:1, y:1, val:0}]))
+    .x(d => d.x, xCat)
+    .y(d => d.y, yCat)
+    .attr('fill', '#888888')
+  }
+
+  public confusionMatrix(data :{
+    tpr: number, 
+    fnr: number, 
+    fpr: number, 
+    tnr: number
+  }, frame:  HTMLElement | string) {
+    const {tpr, fnr, fpr, tnr} = data;
+    const PRED_TRUE = "Predicted True";
+    const PRED_FALSE =  "Predicted False";
+    const ACTUAL_TRUE = "Actual True";
+    const ACTUAL_FALSE = "Actual False";
+    const [xCat, yCat] = this.buildCategoryScale([PRED_TRUE, PRED_FALSE], [ACTUAL_TRUE, ACTUAL_FALSE]);
+    const modifiedData :{x: string, y:string, val: number}[] = [
+      { x: PRED_TRUE, y: ACTUAL_TRUE, val: tpr },
+      { x: PRED_FALSE, y: ACTUAL_TRUE, val: fnr },
+      { x: PRED_TRUE, y: ACTUAL_FALSE, val: fpr },
+      { x: PRED_FALSE, y: ACTUAL_TRUE, val: tnr }
+    ];
+    const colorScale = new Scales.InterpolatedColor();
+    colorScale.range(["#BDCEF0", "#5279C7"])
+    
+    const xAxis = new Axes.Category(xCat, "bottom")
+    const yAxis = new Axes.Category(yCat, "left")
+    const label1 = new Components.Label("ASU")
+    const matrix = new Plots.Rectangle().addDataset(new Dataset(modifiedData))
+    .x(d=> d.x, xCat)
+    .y(d=> d.y, yCat)
+    .attr("fill", d => d.val, colorScale)
+    
+    const masterChart = new Components.Table([
+      [yAxis, matrix],
+      [null, xAxis],
+    ])
+    masterChart.renderTo(frame);
+    return masterChart
+  }
+
+  public confusionMatrixV2(data :{
+    tpr: number, 
+    fnr: number, 
+    fpr: number, 
+    tnr: number
+  }, frame:  HTMLElement | string) {
+    const {tpr, fnr, fpr, tnr} = data;
+    const PRED_TRUE = "Predicted True";
+    const PRED_FALSE =  "Predicted False";
+    const ACTUAL_TRUE = "Actual True";
+    const ACTUAL_FALSE = "Actual False";
+    const xLabel = new Components.Label('PREDICTION').padding(30).addClass('textModColor')
+    const yLabel = new Components.Label('ACTUAL', 90).padding(30)
+    const yLabelAT = new Components.Label(ACTUAL_TRUE, 90)
+    const yLabelAF = new Components.Label(ACTUAL_FALSE, 90)  
+
+    const xLabelPT = new Components.Label(PRED_TRUE);
+    const xLabelPF = new Components.Label(PRED_FALSE);
+    
+    const padding = 50;
+    const tprLabel = new Components.Label(tpr.toString()).padding(padding).addClass('modLabel')
+    const fnrLabel = new Components.Label(fnr.toString()).padding(padding).addClass('modLabel')
+    const fprLabel = new Components.Label(fpr.toString()).padding(padding).addClass('modLabel')
+    const tnrLabel = new Components.Label(tnr.toString()).padding(padding).addClass('modLabel')
+    const group2 = new Components.Table([
+      [yLabelAT, tprLabel, fprLabel],
+      [yLabelAF, fnrLabel, tnrLabel],
+      [null, xLabelPT, xLabelPF],
+    ]).addClass('modTable')
+    const masterTitle = new Components.TitleLabel("CONFUSION MATRIX")
+    const masterChart = new Components.Table([
+      [null, masterTitle],
+      [yLabel, group2],
+      [null, xLabel]
+    ])
+    masterChart.renderTo(frame);
+    return masterChart
+  }
+
+  private generateRandomScatterData(dataSize: number) :{x: number, y: number, size: number}[] {
+    const data = []
+    for (let i=0; i<dataSize; i++) {
+      data.push({
+        x: Math.random(),
+        y: Math.random(),
+        size: Math.random()
+      })
+    }
+    return data
+  }
+
+  public randomScatter(frame: HTMLElement | string) {
+    let data = this.generateRandomScatterData(200)
+    const [ xScale, yScale ] = this.buildXYScale()
+    let dataset = new Dataset(data)
+    const plot = new Plots.Scatter()
+      .addDataset(dataset)
+      .x(function(d) { return d.x; }, xScale)
+      .y(function(d) { return d.y; }, yScale)
+      .size(function(d) { return d.size*100})
+      // this is for SVG Attributes
+      .attr("fill-opacity", .4)
+
+    const colorLabel = new Components.Label("Color")
+    const randomizeLabel = new Components.Label("Random")
+    const panel = new Components.Table([
+      [colorLabel],
+      [randomizeLabel]
+    ])
+    
+    const interactionClick = new Interactions.Click();
+    interactionClick.onClick(() => {
+      plot.removeDataset(dataset)
+      dataset = new Dataset(this.generateRandomScatterData(100))
+      plot.addDataset(dataset)
+    })
+    interactionClick.attachTo(randomizeLabel)
+
+    const interactionClick2 = new Interactions.Click();
+    interactionClick2.onClick(() => {
+      const color = ['#374025', '#D8D9C7', '#A6896F', '#8C594D', '#592B27'];
+      plot.selections().attr('fill', color[Math.floor(Math.random() * color.length)])
+    })
+    interactionClick2.attachTo(colorLabel)
+
+    const xAxis = new Axes.Numeric(xScale, "bottom");
+    const yAxis = new Axes.Numeric(yScale, "left");
+    const masterChart = new Components.Table([
+      [yAxis, plot, panel],
+      [null, xAxis, null]
+    ])
+    masterChart.renderTo(frame)
+    return masterChart
+  }
+
+  private resizeData(data: {x: number, y: number}[], multiplier: number) {
+    return data.map(d => {
+      return {
+        x: d.x * multiplier,
+        y: d.y * multiplier
+      }
+    })
+  }
+
+  public animationScatter(frame: HTMLElement | string) {
+    let data = this.generateRandomScatterData(200)
+    const [ xScale, yScale ] = this.buildXYScale()
+    let dataset = new Dataset(data)
+    const plot = new Plots.Scatter()
+      .addDataset(dataset)
+      .x(function(d) { return d.x; }, xScale)
+      .y(function(d) { return d.y; }, yScale)
+      .size(function(d) { return d.size * 100; })
+      .animated(true)
+      .animator("test",new Animators.Easing().easingMode("bounce"));
+    const [ xAxis, yAxis ] = this.buildXYAxis({xScale, yScale})
+    
+    const interactionClick = new Interactions.Click();
+    interactionClick.onClick(() => {
+      plot.removeDataset(dataset)
+      dataset = new Dataset(this.generateRandomScatterData(100))
+      plot.addDataset(dataset)
+    })
+    interactionClick.attachTo(plot)
+    const masterChart = new Components.Table([
+      [yAxis, plot],
+      [null, xAxis]
+    ])
+    masterChart.renderTo(frame)
+    return masterChart
+  }
+
+  public animateLinePlot(frame: HTMLElement | string) {
+    const data_1 = [
+      {
+        x: 0,
+        y: 0
+      },
+      {
+        x: 4,
+        y: 4
+      }
+    ];
+    const data_2 = [
+      {
+        x: 0,
+        y: 1
+      },
+      {
+        x: 4,
+        y: 3
+      }
+    ]
+    const [ xScale, yScale ] = this.buildXYScale();
+    let newDataset = new Dataset(data_1);
+    let currentData = data_1;
+    const plot = new Plots.Line()
+      .addDataset(newDataset)
+      .x(d => d.x, xScale)
+      .y(d => d.y, yScale)
+      .animated(true)
+      .animator("lineAnim", new Animators.Easing().easingMode("bounce"))
+    const plot2 = new Plots.Line()
+      .addDataset(newDataset)
+      .x(d => d.x, xScale)
+      .y(d => d.y, yScale)
+      .animated(true)
+      .animator("lineAnim", new Animators.Easing().easingMode("bounce"))
+    const [ xAxis, yAxis ] = this.buildXYAxis({xScale, yScale})
+    const interactionClick = new Interactions.Click();
+    interactionClick.onClick(() => {
+      plot.removeDataset(newDataset)
+      currentData = currentData == data_1 ? data_2 : data_1
+      newDataset = new Dataset(currentData)
+      plot.addDataset(newDataset)
+    });
+
+    interactionClick.attachTo(plot)
+    const plotGroup = new Components.PlotGroup([plot, plot2])
+    const masterChart = new Components.Table([
+      [yAxis, plotGroup],
+      [null, xAxis]
+    ])
+    masterChart.renderTo(frame)
+    return masterChart
+  }
+
+  private createBarStairs(addition) {
+    const result = [];
+    for (let i=0; i< 10; i++) {
+      result.push({
+        x: i + addition,
+        y: i+1 + addition
+      })
+    }
+    return result
+  }
+  public plotBarAnimation(frame: HTMLElement | string) {
+    const data_1 = this.createBarStairs(0)
+    const data_2 = this.createBarStairs(3)
+    const [ xScale, yScale ] = this.buildXYScale();
+    let newDataset = new Dataset(data_1);
+    let currentData = data_1;
+    const plot = new Plots.Bar()
+      .addDataset(newDataset)
+      .x(d => d.x, xScale)
+      .y(d => d.y, yScale)
+      .animated(true)
+      .animator("lineAnim", new Animators.Easing().easingMode("bounce"))
+    const [ xAxis, yAxis ] = this.buildXYAxis({xScale, yScale})
+    const interactionClick = new Interactions.Click();
+    interactionClick.onClick(() => {
+      plot.removeDataset(newDataset)
+      currentData = currentData == data_1 ? data_2 : data_1
+      newDataset = new Dataset(currentData)
+      plot.addDataset(newDataset)
+    });
+
+    interactionClick.attachTo(plot)
+    const masterChart = new Components.Table([
+      [yAxis, plot],
+      [null, xAxis]
+    ])
+    masterChart.renderTo(frame)
+    return masterChart
   }
 }
